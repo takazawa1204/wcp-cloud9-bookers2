205028fea224b90a6f20828845a6445d5ce4dac3
takazawa1204 <ckicken1204@gmail.com>
2021-11-22 01:18:25 +0000

    header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root to: 'homes#top'
   devise_for :users
+  get '/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :update, :destroy]
+  
+  resources :users, only: [:index, :show, :edit, :update]
 end
